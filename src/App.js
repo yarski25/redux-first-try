539cb73b0660b6d966db0b95b8e437eb5d9f315d
yarski25 <yaroslav.saprykin@gmail.com>
2023-02-02 11:58:26 +0100

    Redux fetch_posts implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React, { Component } from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 
-function App() {
-  return (
-    <div className="App">
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
-  );
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    };
-  }
-
   componentWillMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     posts: [],
+  //   };
+  // }
+
+  // componentWillMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => this.setState({ posts: data }));
+  // }
 
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -30,4 +36,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
